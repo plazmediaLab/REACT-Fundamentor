d53fdb6d62da5090d9d9e1802cd0ff34384a73a5
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-01-19 17:24:49 -0600

    Que son las PROPS en un componente React

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// [PROPS] -> Se puede tomar las props como tubos por los que alimentarmos a nuestros componentes.
+
+
 // Elemento físico en el DOM.
 const root = document.getElementById("root");
+//Valor para un template String
+let name = 'Evan Alain';
 
-let titulo = 'Docena de manzanas'
-
-// 1.- Estructura de nuestro componente alojado en un función.
-const FruitCard = () => (
+// 1.- Se recibirán los PROPS a nuestro componente a traves de un paramero.
+const FruitCard = (props) => (
   <div>
-    <h3>Titulo: { titulo }</h3>
-    <hr />
+    <h3>Titulo: { props.name }</h3>
     <p>Descripción...</p>
+    <p>Precio: ${props.price}</p>
+    <hr />
   </div>
 );
 
-// 2.- Componente [App] que renderizaremos en el DOM
-// Llamremos a nuestro componente tomandolo como una etiqueta HTML
+// 2.- Los PROPS llegan al componente a traves de propiedades, recibiendo el valor.
+// --- Para valores de tipo [string], son requeridas las comillas simples o dobles, quedando opcionales las llaves.
+// --- Para valores de tipo [num], es requerido las llaves para introducir el valor.
+// --- Para los Template String, son requeridas las llaves.
 const App = () => (
   <div>
-    <FruitCard></FruitCard>
+    <FruitCard name='Manzana' price={3.50}></FruitCard>
+    <FruitCard name="Fresa" price={2.99}></FruitCard>
+    <FruitCard name={'Mango'} price={5.99}></FruitCard>
+    <FruitCard name={`Mi nombre es ${ name }`} price={999.99}></FruitCard>
   </div>
 );
 
-// 3.- Para poder mandar nuestro componente al DOM
-// Lo tenemos que llamar como parametro, de manera a una etiqueta.
+
 ReactDOM.render(<App />, root);
