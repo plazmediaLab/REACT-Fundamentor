36919b91ce102cad1a97ac0c8a7ab8210ac17cfc
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-01-19 16:08:02 -0600

    Crear un elemento y montarlo en el DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+// 1.- Inportamos [React] desde la librería npm [react].
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// 2.- Inportamos [ReactDOM] desde la librería npm [react].
+// Esta librería es la encargada de renderizar los elementos en el DOM.
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 3.- Creamos nuestro elemento a renderizar
+const App = <h1>Hello Worl!!!</h1>;
+// Babel será el encargado de compilar el elemento.
+// const App = React.createElement("h1", null, "Hello Worl!!!");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 4.- Accedemos al elemento HTML del DOM en el cual renderizaremos nuetsra app.
+// -> ./public/index.html
+const root = document.getElementById('root');
+
+// 5.- Mandamos renderizar nuestro elemento a traves del metodo [.render()].
+// Este recibe dos parametros principales.
+//    1{ Elemento a renderizar } 
+//    2{ Elemento real del DOM en donde se rendirazara } 
+ReactDOM.render(App, root);
