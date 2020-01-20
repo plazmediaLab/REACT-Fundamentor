27f2a1a73b1a994f0c893596e249c81e7c97a85d
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-01-20 16:05:36 -0600

    Inicializadores de propiedad (property Initializers)

--- src/index.js
+++ src/index.js
@@ -6,63 +6,30 @@ import ReactDOM from "react-dom";
 const root = document.getElementById("root");
 
 class ProductsGroup extends React.Component {
-  constructor() {
-    super();
-
-    /*=================================================================================
-     *	 Aún que podemos agregar [onClick={this.addQuantity.bind(this)}] directamente
-     *  en el punto del evento dentro de la etiqueta <button> como propiedad.
-     *  Es buena practica declarar el [bind] dentro del metodo constructor, de esta
-     *  manera servira para todos los metodos aplicados en nuestro componente.  
-     *  Aún que por el momento esta declaración esta bien, al aumentar el número
-     *  de [bind] utilizados, esto se comvertiria muy complicado de mantener */
-    // this.addQuantity = this.addQuantity.bind(this);
-    // this.subtractQuantity = this.subtractQuantity.bind(this);
-    /*=================================================================================
-    *	 Una mejor practica es crear un array con los string de los metodos a procesar
-    *  con el metodo [bind()] y aplicar un ciclo [forEach()]  */
-    const METHODS = [
-      'addQuantity',
-      'subtractQuantity',
-      'cleanQuantity'
-    ];
-    METHODS.forEach((method_arg) => {
-      this[method_arg] = this[method_arg].bind(this);
-    })
-
-    this.state = {
-      quantity: 0
-    };
-  }
 
   /*=================================================================================
-   *  NOTA:
-   *	 Para majear de forma mas optima las funciones a realizar tras el evento CLICK
-   *	 crearemos la función por fuera del metodo render a diferencia del manejo de
-   *	 funciones de flecha, al evitar usar directamente estas funciones, react tendra
-   *  procesos mas eficientes
-   * --------------------------------------------------------------------------------*/
-  addQuantity() {
-    this.setState({
-      quantity: this.state.quantity + 1
-    });
-  }
-  subtractQuantity() {
-    this.setState({
-      quantity: this.state.quantity - 1
-    });
-  }
-  cleanQuantity() {
-    this.setState({
-      quantity: 0
-    });
-  }
+  *	 Otra manera de actuar ante el metodo [bind()], es presidir de el, por lo que 
+  *  aremos nuestras funciones en funciones arrow, eliminando igualmente el metodo
+  *  [constructor()] y dejando por fuera el [state]  */
+  state = {
+    quantity: 0
+  };
 
   /*=================================================================================
-   *	 NOTA: El metodo [bind()] nos ayuda a canviar el contexto de la palabra reservada
-   *  [this] dentro de nuestra función, así react podra procesar esta llamada sobre
-   *  la clase y su estado, ya que ahí es en donde esta nuestro dato a modificar
-   * --------------------------------------------------------------------------------*/
+  *	 Modificadas a Arrow funtions  */
+  addQuantity = () => this.setState({
+    quantity: this.state.quantity + 1
+  });
+  subtractQuantity = () => this.setState({
+    quantity: this.state.quantity - 1
+  });
+  cleanQuantity = () => this.setState({
+    quantity: 0
+  });
+
+  
+  /*=================================================================================
+  *	 Metodo que renderizara la estructura del componente  */
   render() {
     return (
       <div>
