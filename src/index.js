88ba77748ae299de62f92ec11e0107b3eb68d3c4
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-01-20 15:49:27 -0600

    Usando métodos de clase para manejar EVENTOS

--- src/index.js
+++ src/index.js
@@ -2,80 +2,93 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 /*=================================================================================
-*	  Elemento físico en el DOM  */
-const root = document.getElementById('root');
+ *	  Elemento físico en el DOM  */
+const root = document.getElementById("root");
 
-/*=================================================================================
-*	 NOTA: La principal diferencia entre componentes construidos a partir de una función y una clase.
-*  es que los componentes en clase, se puden modificar y actualizar a ellos mismos
-*  ya que poseen un [estado].
-* --------------------------------------------------------------------------------*/
-
-/*=================================================================================
-*	 ESTADO: representa el momento actual del componente, si el estado canbia el componente tambíen debería hacerlo.
-* --------------------------------------------------------------------------------*/
-
-/*=================================================================================
-*	 Componentes creados a partir de una clase extendida de [React.Component]
-*	 Utilizaremos su parametro [render()], retornando nuestra estructura JSX.
-*	 Lor PROPS en este metodo los recibiremos a traves de la palabra reseravada [this].
-* --------------------------------------------------------------------------------*/
- 
-class ProductsGroup extends React.Component{
-  /*=================================================================================
-  *	 El metodo [constructor] se inicializa en el momento que se crea el componente.
-  *	 Dentro de este es donde se establece el estado del componente.
-  * --------------------------------------------------------------------------------*/ 
-  constructor(){
+class ProductsGroup extends React.Component {
+  constructor() {
     super();
 
     /*=================================================================================
-    *	 Propiedades establecidas en el estado del componente.
-    * --------------------------------------------------------------------------------*/
+     *	 Aún que podemos agregar [onClick={this.addQuantity.bind(this)}] directamente
+     *  en el punto del evento dentro de la etiqueta <button> como propiedad.
+     *  Es buena practica declarar el [bind] dentro del metodo constructor, de esta
+     *  manera servira para todos los metodos aplicados en nuestro componente.  
+     *  Aún que por el momento esta declaración esta bien, al aumentar el número
+     *  de [bind] utilizados, esto se comvertiria muy complicado de mantener */
+    // this.addQuantity = this.addQuantity.bind(this);
+    // this.subtractQuantity = this.subtractQuantity.bind(this);
+    /*=================================================================================
+    *	 Una mejor practica es crear un array con los string de los metodos a procesar
+    *  con el metodo [bind()] y aplicar un ciclo [forEach()]  */
+    const METHODS = [
+      'addQuantity',
+      'subtractQuantity',
+      'cleanQuantity'
+    ];
+    METHODS.forEach((method_arg) => {
+      this[method_arg] = this[method_arg].bind(this);
+    })
+
     this.state = {
       quantity: 0
-    }
+    };
   }
 
-  render(){
+  /*=================================================================================
+   *  NOTA:
+   *	 Para majear de forma mas optima las funciones a realizar tras el evento CLICK
+   *	 crearemos la función por fuera del metodo render a diferencia del manejo de
+   *	 funciones de flecha, al evitar usar directamente estas funciones, react tendra
+   *  procesos mas eficientes
+   * --------------------------------------------------------------------------------*/
+  addQuantity() {
+    this.setState({
+      quantity: this.state.quantity + 1
+    });
+  }
+  subtractQuantity() {
+    this.setState({
+      quantity: this.state.quantity - 1
+    });
+  }
+  cleanQuantity() {
+    this.setState({
+      quantity: 0
+    });
+  }
+
+  /*=================================================================================
+   *	 NOTA: El metodo [bind()] nos ayuda a canviar el contexto de la palabra reservada
+   *  [this] dentro de nuestra función, así react podra procesar esta llamada sobre
+   *  la clase y su estado, ya que ahí es en donde esta nuestro dato a modificar
+   * --------------------------------------------------------------------------------*/
+  render() {
     return (
       <div>
-        <h2>Title:{ this.props.name }</h2>
+        <h2>Title:{this.props.name}</h2>
         <p>Description...</p>
-        <p>${ this.props.price }</p>
-        <p>Quantity: { this.state.quantity }</p>
-        <button 
-          onClick={()=>{
-              // this.state.quantity ++;
-              // this.forceUpdate();
-              /*=================================================================================
-              *	 No es buena practica utilizar el metodo [this.forceUpdate()], aún que
-              *	 esta disponible para su utilización.
-              * --------------------------------------------------------------------------------*/
-
-              /*=================================================================================
-              *	 [this.setState()] con este metodo podemos actualizar el estado de nuestro componente
-              * --------------------------------------------------------------------------------*/
-              this.setState({quantity: this.state.quantity + 1})
-            }
-          }
-        >Add</button>
-        <hr/>
+        <p>${this.props.price}</p>
+        <p>Quantity: {this.state.quantity}</p>
+        <button onClick={this.addQuantity}> + </button>
+        <button onClick={this.subtractQuantity}> - </button>
+        <button onClick={this.cleanQuantity}>Clean</button>
+        <hr />
       </div>
-    )
+    );
   }
 }
 
 /*=================================================================================
-*	 Elemento a renderizar en el DOM  */
+ *	 Elemento a renderizar en el DOM  */
 const App = () => (
   <div>
-    <ProductsGroup name={' Mac 21\"'} price={18000.99}></ProductsGroup>
-    <ProductsGroup name={' iWatch'} price={6000.99}></ProductsGroup>
-    <ProductsGroup name={' iPhone'} price={25000.99}></ProductsGroup>
+    <ProductsGroup name={' Mac 21"'} price={18000.99}></ProductsGroup>
+    <ProductsGroup name={" iWatch"} price={6000.99}></ProductsGroup>
+    <ProductsGroup name={" iPhone"} price={25000.99}></ProductsGroup>
   </div>
 );
 
 /*=================================================================================
-*	 Metodo llamado para renderizar el elemento en el DOM */
+ *	 Metodo llamado para renderizar el elemento en el DOM */
 ReactDOM.render(<App />, root);
