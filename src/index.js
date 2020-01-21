c1e242b901a1f5a4e4041a90eada06c95c3a9902
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-01-20 19:08:15 -0600

    Aprendiendo JSX en mas profundidad

--- src/index.js
+++ src/index.js
@@ -12,3 +12,29 @@ const root = document.getElementById("root");
 /*=================================================================================
  *	 Metodo llamado para renderizar el elemento en el DOM */
 ReactDOM.render(<App />, root);
+
+
+/*
+* =================================================================================
+*
+*	 JSX !== HTML
+*
+* --------------------------------------------------------------------------------
+*/
+/*=================================================================================
+*	 JSX es una extención de XML, por lo tamto es estricto en su forma de declarar.
+*  Siempre se tienen que cerrar las etiquetas, ya sea con etiqueta doble de cierre
+*  o cierre en etiqueta unica.
+*
+*     1.- <etiqueta></etiqueta>
+*     2.- <etiqueta />
+* --------------------------------------------------------------------------------*/
+/*=================================================================================
+*	 JSX es azúcar sintáctico, término para referirse a los añadidos a la sintaxis de 
+*  un lenguaje de programación diseñados para hacer algunas construcciones más 
+*  fáciles de leer o expresar.
+* --------------------------------------------------------------------------------*/
+/*=================================================================================
+*	 Los navegadores no comprenden este lenguaje, para resolver esto react utiliza 
+*  Babel para compilar todo el el código
+* --------------------------------------------------------------------------------*/
