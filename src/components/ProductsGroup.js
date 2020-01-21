449ed5b022aecbfffc2683d5b4f452bd7957b210
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-01-21 16:31:05 -0600

    Aplicar estilos con clases CSS en componente de React

--- src/components/ProductsGroup.js
+++ src/components/ProductsGroup.js
@@ -2,6 +2,10 @@
 *	 Todo componente independiente requiere inportar [REACT]  */
 import React from 'react';
 
+/*=================================================================================
+*	 Importación de archivo CSS  */
+import './ProductsGroup.css';
+
 class ProductsGroup extends React.Component {
 
   state = {
@@ -24,33 +28,30 @@ class ProductsGroup extends React.Component {
   render() {
 
     /*=================================================================================
-    *	 Los estilos dentro de nuestro componente los podemos declarar en línea dentro
-    *  de la estructura JSX, pero esto lo volvera poco sostonible a futuro, por lo que
-    *  otro metodo es es declararlo en una constante a modo de objeto con las propiedades
-    *  y su respectivo valor  */
+    *	 Podemos separar las clases CSS en archivos independientes para cada componente
+    *  como en cualquier proyecto web, con la diferencia de que en react tenemos que 
+    *  importar este documento CSS en cado de estar dentro de nuestro proyecto, para 
+    *  así ser compilado por webpack.
+    * --------------------------------------------------------------------------------*/
     /*=================================================================================
-    *	 Para esto en el nombre de las propiedades implementaremos CamelCase, ya que 
-    *  utilizar guin medio, como es usual en CSS no funcionara  */
-
+    *	 Una ves tenemos acceso a este archivo, lo podemos agregar a la estructura de 
+    *  nuestro componente, pero no podemos utilizar la palabra reserbada [class], como
+    *  lo hariamos en HTML, por lo tanto tenemos que hacerlo a tra ves de la palabra
+    *  reservada [className].
+    * --------------------------------------------------------------------------------*/
     /*=================================================================================
-    *	 Igualmente dentro de estas configuraciones podemos implementar lógica de programación
-    *  Ya sea directamente, declarada o a modo de constantes y (0) varibles
-    *  como en el siguiente caso con una condicional terciaria.  */
-    const dinamicData = this.state.quantity > 0;
-
-    const style = {
-      padding: '2em',
-      border: '1px solid #d0d0d0',
-      borderRadius: '1em',
-      marginBottom: '1em',
-      boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.15)',
-      transition: 'all 400ms ease-out',
-      background: dinamicData ? 'linear-gradient(0deg, rgb(32,124,229), rgb(73,155,234)': '#fff',
-      color: dinamicData ? '#fff' : '#000',
-    }
+    *	 Igualmente se puede implementar logíca intermedia en para el manejo de estas clases  */
+    // const dinamicClassCss = this.state.quantity > 0 ? 'ProductsGroupActive' : 'ProductsGroup';
+    
+    /*=================================================================================
+    *	 Un metodo para agregar mas de una clase utilizando logíca, es utilizar template string  */
+    const hasQuantity = this.state.quantity > 0
+    const dinamicClassCss = `ProductsGroup ${hasQuantity ? 'ProductsGroup-active' : ''}`;
 
     return (
-      <div style={style}>
+      /*=================================================================================
+      *	 Palabra reservada [className]  */
+      <div className={dinamicClassCss}>
         <h2>Title:{this.props.name}</h2>
         <p>Description...</p>
         <p>${this.props.price}</p>
